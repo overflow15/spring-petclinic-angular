c65a5278001da9ec00bc0e58620f5034e43576f9
overflow15 <58467720+overflow15@users.noreply.github.com>
2020-02-03 11:40:56 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -19,13 +19,18 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
     },
-    
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers: ['ChromeHeadless'],
+    singleRun: true
   });
 };
